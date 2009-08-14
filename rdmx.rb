d597f3ded82d00d56b0784a6210598165ef6bbf0
Ian Smith-Heisters <i@idiosyncra.tc>
2009-08-14 16:56:52 -0700

    Reorg

--- rdmx.rb
+++ rdmx.rb
@@ -1,13 +1,11 @@
-#!/usr/bin/env ruby
-
 require 'rubygems'
 
-module Rdmx
-end
+module Rdmx; end
+
 %w(
-  rdmx/dmx
-  rdmx/universe
-  rdmx/fixture
-  rdmx/animation
-  rdmx/layers
-).each{|r|require File.dirname(__FILE__)+"/#{r}"}
+  dmx
+  universe
+  fixture
+  animation
+  layers
+).each{|r|require File.dirname(__FILE__)+"/lib/#{r}"}
